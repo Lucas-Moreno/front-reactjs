fe5456a83d25415ab114f18764aa58639b6bbdb1
Lucas-Moreno <moreno.lucas.82300@hotmail.fr>
2023-02-04 16:33:18 +0100

    chore: architecture front

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from '@material-ui/core'
+
+function App() {
+  return (
+    <div >
+      <Button variant="contained" color="primary">
+        hello world
+      </Button>
+    </div>
+  );
+}
+
+export default App;
