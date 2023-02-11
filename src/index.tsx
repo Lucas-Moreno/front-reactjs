54068da2cd17dd17f8328de3514db1360a471c49
Lucas-Moreno <moreno.lucas.82300@hotmail.fr>
2023-02-11 17:32:33 +0100

    feat: change index.tsx react 18 to 17

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+import React from 'react'
+import { render } from 'react-dom'
+import App from './App'
+
+const root = document.getElementById('root') as HTMLElement
+render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  root
 );
 
